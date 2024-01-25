001b70baaf82e0bc414269038075cb277fe99c33
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2024-01-25 15:48:25 +0500

    feat: complaint details page

--- src/pages/Complaints/Complaints.tsx
+++ src/pages/Complaints/Complaints.tsx
@@ -1,37 +1,95 @@
-import { Box, Typography } from "@mui/material";
-import StickyHeadTable from "../../components/StickyHeaderTable/StickyHeaderTable";
-import { useNavigate } from "react-router-dom";
-import { COLUMNS, ROUTES, SEARCH_PARAMS } from "../../config/constants";
+import { Box, Button, Typography } from "@mui/material";
+import { useParams } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../config/store";
-import { Data } from "../../types/complaint";
-import { formatRows } from "../../utils";
-import { useEffect } from "react";
-import { getComplaintsThunk } from "../../features/complaint/complaintSlice";
+import { useCallback, useEffect } from "react";
+import {
+  getComplaintsThunk,
+  updateComplaintStatusThunk,
+} from "../../features/complaint/complaintSlice";
+import { Complaint, ComplaintStatus } from "../../enums";
 
 export default function Complaints() {
-  const navigate = useNavigate();
-  const complaints = useAppSelector((state) => state.complaint.complaints);
+  const { complaintId } = useParams();
+  const complaint = useAppSelector((state) =>
+    state.complaint.complaints.find(
+      (complaint) => complaint._id.toString() === complaintId
+    )
+  );
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(getComplaintsThunk());
   }, []);
 
-  const handleRowClick = (row: Data) => {
-    // navigate(`../${row.id}?${SEARCH_PARAMS.status}=${ROUTES.inProgress}`);
-  };
+  const handleClick = useCallback(() => {
+    dispatch(
+      updateComplaintStatusThunk({
+        id: complaint?._id!,
+        status:
+          complaint?.status === ComplaintStatus.Pending
+            ? ComplaintStatus.InProgress
+            : complaint?.status === ComplaintStatus.InProgress
+            ? ComplaintStatus.Completed
+            : complaint?.status === ComplaintStatus.Completed
+            ? ComplaintStatus.Archived
+            : ComplaintStatus.Archived,
+      })
+    );
+  }, [complaint]);
 
   return (
-    <Box p={4}>
-      <Typography variant="h4">Complaints</Typography>
-      <StickyHeadTable
-        columns={COLUMNS}
-        rows={formatRows(complaints || [])}
-        onRowClick={handleRowClick}
-        sx={{
-          mt: 5,
-        }}
-      />
+    <Box p={4} display={"flex"} flexDirection={"column"} gap={2}>
+      <Typography variant="h4">Complaint Details</Typography>
+      <Box
+        border={2}
+        borderColor={"primary.main"}
+        display={"flex"}
+        flexDirection={"column"}
+        gap={1}
+        p={2}
+        borderRadius={2}
+      >
+        <Box display={"flex"} gap={1}>
+          <Typography>Complaint no:</Typography>
+          <Typography fontWeight={600}>{complaint?._id}</Typography>
+        </Box>
+        <Box display={"flex"} gap={1}>
+          <Typography>Type:</Typography>
+          <Typography fontWeight={600}>
+            {Complaint[complaint?.type!]}
+          </Typography>
+        </Box>
+        <Box display={"flex"} gap={1}>
+          <Typography>Status:</Typography>
+          <Typography fontWeight={600}>
+            {ComplaintStatus[complaint?.status!]}
+          </Typography>
+        </Box>
+        <Box display={"flex"} gap={1}>
+          <Typography>Block:</Typography>
+          <Typography fontWeight={600}>{complaint?.block}</Typography>
+        </Box>
+        <Box display={"flex"} gap={1}>
+          <Typography>Address:</Typography>
+          <Typography fontWeight={600}>{complaint?.house}</Typography>
+        </Box>
+        <Box display={"flex"} gap={1}>
+          <Typography>Date:</Typography>
+          <Typography fontWeight={600}>
+            {new Date(complaint?.date!).toLocaleString()}
+          </Typography>
+        </Box>
+        <Button variant="contained" onClick={handleClick}>
+          Mark as{" "}
+          {complaint?.status === ComplaintStatus.Pending
+            ? "In Progress"
+            : complaint?.status === ComplaintStatus.InProgress
+            ? "Completed"
+            : complaint?.status === ComplaintStatus.Completed
+            ? "Archived"
+            : ""}
+        </Button>
+      </Box>
     </Box>
   );
 }
--- src/services/complaints.ts
+++ src/services/complaints.ts
@@ -1,9 +1,23 @@
 import { api } from "../config/axios";
+import { ComplaintStatus } from "../enums";
 import { Complaint } from "../types/complaint";
 
 const complaintRoute = "complaints";
 
 export const getAllComplaints = async () => {
   const { data } = await api.get<Complaint[]>(`/${complaintRoute}/`);
+  return data;
+};
+
+export const updateComplaintStatus = async (
+  id: string,
+  status: ComplaintStatus
+) => {
+  const { data } = await api.put<Complaint, any, Partial<Complaint>>(
+    `/${complaintRoute}/${id}`,
+    {
+      status,
+    }
+  );
   return data;
 };
