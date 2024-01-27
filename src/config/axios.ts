7c2aedd48ed3605ec8c2d1ed95e1678120d9bd09
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2024-01-27 09:17:50 +0500

    chore: production server ulr added

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL:
-    process.env.NODE_ENV === "production" ? "" : "http://localhost:3000/",
+    process.env.NODE_ENV === "production"
+      ? "https://calm-crag-67242-9c4818c543b7.herokuapp.com/"
+      : "http://localhost:3000/",
   headers: {
     common: {
       Authorization: `Bearer ${
